db34fa63da6fd9845b33ea6ea15f327d1dd3f154
nimagrolis <486016@gmail.com>
2022-11-15 17:02:31 +0300

    v5.5.3 Update for binexe

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const util = require('util');
 
-const plugin = require('ih-plugin-api')();
+// const plugin = require('ih-plugin-api')();
 const app = require('./app');
 
 (async () => {
-  plugin.log('Email plugin has started.', 0);
-
+  let plugin;
   try {
+    const opt = getOptFromArgs();
+    const pluginapi = opt && opt.pluginapi ? opt.pluginapi : 'ih-plugin-api';
+    plugin = require(pluginapi + '/index.js')();
+
+    plugin.log('Email plugin has started.', 0);
+
     // Получить параметры
     plugin.params.data = await plugin.params.get();
-    plugin.log('Received params '+util.inspect(plugin.params.data));
-    
+    plugin.log('Received params ' + util.inspect(plugin.params.data));
+
     app.start(plugin);
   } catch (err) {
     plugin.exit(8, `Error: ${util.inspect(err)}`);
   }
 })();
+
+function getOptFromArgs() {
+  let opt;
+  try {
+    opt = JSON.parse(process.argv[2]); //
+  } catch (e) {
+    opt = {};
+  }
+  return opt;
+}
